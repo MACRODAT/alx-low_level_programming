d307c8cab70ce293458c868a480e950eae011786
macrodat <macrodatservices@gmail.com>
2023-07-14 14:25:14 +0100

    nowth

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,14 +11,10 @@ int main(void)
 
 	for (d1 = 0; d1 < 10; d1++)
 	{
-		for (d2 = 0; d2 < 10; d2++)
+		for (d2 =d1 + 1; d2 < 10; d2++)
 		{
 			putchar((d1 % 10) + '0');
 			putchar((d2 % 10) + '0');
-
-			if (d1 == 9 && d2 == 9)
-				continue;
-			
 			putchar(',');
 			putchar(' ');
 		}
