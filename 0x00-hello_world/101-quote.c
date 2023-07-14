e07363af417339b30d915c9418cc39b341f38321
macrodat <macrodatservices@gmail.com>
2023-07-14 01:21:11 +0100

    bsje

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <unistd.h>
 #include <string.h>
 
-/*
+/**
  * main - prints error
  * return: 0
  */
@@ -9,5 +9,5 @@ int main(void){
 	char s[]= "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	write(2, s, strnlen(s, sizeof(s)));
 	
-	return (0);
+	return (1);
 }
