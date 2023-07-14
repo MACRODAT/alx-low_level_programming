ca8a3d209b4a39ce4abe58974845fcd45f28a745
macrodat <macrodatservices@gmail.com>
2023-07-14 01:22:53 +0100

    bsje

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,9 +5,10 @@
  * main - prints error
  * return: 0
  */
-int main(void){
-	char s[]= "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, s, strnlen(s, sizeof(s)));
-	
+int main(void)
+{
+	char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, s, strnlen(s, sizeof(s)));	
 	return (1);
 }
