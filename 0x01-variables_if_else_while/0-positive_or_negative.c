55d7cfa2941384305b46c2b81c2b6a86d05b5497
macrodat <macrodatservices@gmail.com>
2023-07-14 13:19:38 +0100

    bsje

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+
+/* main */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX/2;
+	if (n > 0)
+	{
+		printf("is positive\n");
+	}
+	else if(n < 0)
+	{
+		printf("is negative\n");
+	}
+	else
+	{
+		printf("is zero");
+	}
+	
+	return (0);
+}
