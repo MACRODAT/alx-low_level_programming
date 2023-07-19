f27c9cc67e9039ac27819072bc6c1ac881a995d2
macrodat <macrodatservices@gmail.com>
2023-07-19 04:18:04 +0100

    nowth

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - prints 1024
+ *
+ * Return: void
+ */
+
+int main(void)
+{
+	int cnt;
+	unsigned long fib1 = 0, fib2 = 1, sum;
+
+	for (cnt = 0; cnt < 50; cnt++)
+	{
+		sum = fib1 + fib2;
+		printf("%lu", sum);
+
+		fib1 = fib2;
+		fib2 = sum;
+
+		if (cnt == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+
+	return (0);
+}
