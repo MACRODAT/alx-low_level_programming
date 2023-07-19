47ace6647d25716d951861e6bcd575c371ac9d9b
macrodat <macrodatservices@gmail.com>
2023-07-19 02:19:04 +0100

    nowth

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - main fct
+ * @c: char
+ * Return: 0
+ */
+
+int  _isaplha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	return (0);
+}
