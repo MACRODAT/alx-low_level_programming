04ae231b4f8e5d50af31ccde2c0b2a155b01c308
macrodat <macrodatservices@gmail.com>
2023-07-19 01:59:12 +0100

    nowth

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - main fct
+ * Return: 0
+ */
+
+int main(void)
+{
+	int ind = 0;
+
+	while (ind < 26)
+	{
+		_putchar('a' + ind);
+		ind++;
+	}
+	_putchar('\n');
+	return (0);
+}
+
