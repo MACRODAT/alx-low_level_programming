00198ccf5dee2ce580acab074475ea385a351e02
macrodat <macrodatservices@gmail.com>
2023-07-19 02:30:08 +0100

    nowth

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - main fct
+ * @n: number
+ * Return: 0
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n <= 0)
+	{
+		return (-n);
+	}
+}
