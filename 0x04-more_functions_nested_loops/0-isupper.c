8bc18e0811895d87dc5b367e38befefcbc2942b0
macrodat <macrodatservices@gmail.com>
2023-07-20 04:52:44 +0100

    alx

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isupper - uppercase
+ * @c: character
+ * Return: 001
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isdigit - check dgt
+ * @c: int
+ * Return: 1or0
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
