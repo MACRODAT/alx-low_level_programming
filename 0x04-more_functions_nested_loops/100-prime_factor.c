c7209bcc0f8db8e4768b789f4bbf24be1ea71749
macrodat <macrodatservices@gmail.com>
2023-07-20 21:18:03 +0100

    correction 100

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,25 +8,27 @@
 int main(void)
 {
 	long long n;
-	long int div=2, ans = 0, maxFact;
+	long long max;
+	long long i;
 
-    n = 612852475143;
+	n = 612852475143;
+	//n = 101;
+	max = -1;
 
-    while (n != 0)
-    {
-        if (n % div != 0)
-            div = div + 1;
-        else
-        {
-            maxFact = n;
-            n = n / div;
-            if (n == 1) {
-                printf("%ld",maxFact);
-                ans = 1;
-                break;
-            }
-        }
-    }
+	for (i = 3; i <= sqrt(n); i = i + 2)
+	{
+		while (n % i == 0)
+		{
+			max = i;
+			//printf("%ld\n", max);
+			n = n / i;
+		}
+	}
+
+	if (n > 2)
+		max = n;
+
+	printf("%ld\n", max);
 
 	return (0);
 }
