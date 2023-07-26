38f5b964c96a4804b91a620f19ddea485b857203
macrodat <macrodatservices@gmail.com>
2023-07-26 15:02:05 +0100

    pointers ex

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_number - prints an integer
+ * @n:.input integer parameter
+ *
+ *
+ */
+void print_number(int n)
+{
+	unsigned int x = n;
+	if (n < 0)
+	{
+		n = -n;
+	}
+}
