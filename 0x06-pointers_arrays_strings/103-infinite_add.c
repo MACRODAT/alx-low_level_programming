b6d20af942575f0eaaaf507754259b8597efe656
MACRODAT <macrodatservices@gmail.com>
2023-07-27 13:29:24 +0100

    Changes to the file

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,77 +1,85 @@
 #include "main.h"
 
 /**
- * infinite_add - adder
- * @n1: frst
- * @n2: sec
- * @r: buffer
- * @size_r: buffer size
- * Return: pointer
+ * rev_string - Reverses a character array.
+ * @n: The character array to be reversed.
+ * Return: None.
  */
-
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+void rev_string(char *n)
 {
 	int i = 0;
-	int carry = 0;
-	char s1[size_r], s2[size_r];
-	int flag1 = 0;
-	int flag2 = 0;
-	char res[size_r];
+	int j = 0;
+	char temp;
 
-	for (i = 0; i < size_r; ++i)
+	while (*(n + i) != '\0')
 	{
-		if (!flag1 && n1[i] == '\0')
-			flag1 = i;
-		if (flag1 > 0)
-			s1[i] = '\0';
-		else
-			s1[i] = n1[i];
-		if ((i == size_r - 1) && !n1[i + 1] && flag1 == 0)
-			return (0);
-	}
-	for (i = 0; i < size_r; ++i)
-	{
-		if (!flag2 && n2[i] == '\0')
-			flag2 = i;
-		if (flag2 > 0)
-			s2[i] = '\0';
-		else
-			s2[i] = n2[i];
-		if ((i == size_r - 1) && !n2[i + 1] && flag2 == 0)
-			return (0);
-	}
-
-	r[size_r] = '\0';
-	for (i = 0; i < size_r; ++i)
-	{
-		if (flag1 - 1 - i >= 0)
-			carry += (s1[flag1 - 1 - i] - '0');
-		if (flag2 - 1 - i >= 0)
-			carry += (s2[flag2 - 1 - i] - '0');
-		*(res  + size_r - 1 - i) = '0' + carry % 10;
-		if (i != size_r - 1)
-			carry = carry / 10;
-	}
-	if (carry > 0)
-		return (0);
-	i = 0;
-	carry = 0;
-	flag1 = 0;
-	while (i < size_r)
-	{
-
-		if (res[i] >= '0' && res[i] <= '9')
-			if (res[i] >= '1')
-			{
-				flag1 = 1;
-			}
-			if (flag1)
-			{
-				r[carry] = res[i];
-				carry++;
-			}
 		i++;
 	}
-	r[i] = '\0';
+	i--;
+
+	for (j = 0; j < i; j++, i--)
+	{
+		temp = *(n + j);
+		*(n + j) = *(n + i);
+		*(n + i) = temp;
+	}
+}
+
+/**
+ * infinite_add - Adds two numbers together.
+ * @n1: The text representation of the first number to add.
+ * @n2: The text representation of the second number to add.
+ * @r: Pointer to the buffer where the result will be stored.
+ * @size_r: The size of the buffer.
+ * Return: Pointer to the calling function.
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int overflow = 0, i = 0, j = 0, digits = 0;
+	int val1 = 0, val2 = 0, temp_tot = 0;
+
+	while (*(n1 + i) != '\0')
+		i++;
+	while (*(n2 + j) != '\0')
+		j++;
+	i--;
+	j--;
+
+	if (j >= size_r || i >= size_r)
+		return (0);
+
+	while (j >= 0 || i >= 0 || overflow == 1)
+	{
+		if (i < 0)
+			val1 = 0;
+		else
+			val1 = *(n1 + i) - '0';
+
+		if (j < 0)
+			val2 = 0;
+		else
+			val2 = *(n2 + j) - '0';
+
+		temp_tot = val1 + val2 + overflow;
+
+		if (temp_tot >= 10)
+			overflow = 1;
+		else
+			overflow = 0;
+
+		if (digits >= (size_r - 1))
+			return (0);
+
+		*(r + digits) = (temp_tot % 10) + '0';
+		digits++;
+		j--;
+		i--;
+	}
+
+	if (digits == size_r)
+		return (0);
+
+	*(r + digits) = '\0';
+	rev_string(r);
 	return (r);
 }
