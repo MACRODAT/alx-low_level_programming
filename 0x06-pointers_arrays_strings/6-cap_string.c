b24d90d6de47ba8c3ee4645a3ccd54822228787b
MACRODAT <macrodatservices@gmail.com>
2023-07-27 04:14:35 +0100

    Changes to the file

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,18 +12,18 @@ char *cap_string(char *str)
 	char sep[] = {9, 10, 32, 33, 34, 40, 41, 44, 46, 59, 63, 123, 125};
 
 	i = 0;
-    if (str[i] >= 97 && str[i] <= 122)
+	if (str[i] >= 97 && str[i] <= 122)
 	{
-        str[i] = str[i] - 32;
+		str[i] = str[i] - 32;
 	}
-    i = 1;
+	i = 1;
 	while (str[i] != '\0')
 	{
 		j = 0;
 		while (sep[j] != '\0')
 		{
-            if (sep[j] == str[i] && (str[i + 1] >= 97 && str[i + 1] <= 122))
-            {
+			if ((sep[j] == str[i]) && (str[i + 1] >= 97 && str[i + 1] <= 122))
+			{
 				str[i + 1] = str[i + 1] - 32;
 			}
 			j++;
