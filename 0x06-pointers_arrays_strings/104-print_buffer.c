a8ca220c50ebb61fb1aa189537b516935b27c559
MACRODAT <macrodatservices@gmail.com>
2023-07-28 03:13:24 +0100

    Changes to the file

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,64 +2,41 @@
 #include <stdio.h>
 
 /**
- * print_line - Print 's' bytes of a buffer 'c' starting from line 'l'.
- * @c: The buffer to print.
- * @s: The number of bytes to print.
- * @l: The line of the buffer to print.
- *
- * Return: void.
- */
-void print_line(char *c, int s, int l)
-{
-	int j, k;
-
-	for (j = 0; j <= 9; j++)
-	{
-	if (j <= s)
-		printf("%02x", c[l * 10 + j]);
-	else
-		printf("  ");
-
-		if (j % 2)
-			putchar(' ');
-	}
-
-	for (k = 0; k <= s; k++)
-	{
-		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
-			putchar(c[l * 10 + k]);
-		else
-			putchar('.');
-	}
-}
-
-/**
- * print_buffer - Print the content of a buffer.
- * @b: The buffer to print.
- * @size: The size of the buffer.
- *
- * Return: void.
+ * print_buffer - Displays the contents of a buffer 10 bytes at a time
+ * @b: The buffer to be displayed.
+ * @size: The number of bytes to be displayed from the buffer.
  */
 void print_buffer(char *b, int size)
 {
-	int i;
+    int byte, index;
 
-	for (i = 0; i <= (size - 1) / 10 && size; i++)
-	{
-		printf("%08x: ", i * 10);
+    for (byte = 0; byte < size; byte += 10)
+    {
+        printf("%08x: ", byte);
+        for (index = 0; index < 10; index++)
+        {
+            if ((index + byte) >= size)
+                printf("  ");
+            else
+                printf("%02x", *(b + index + byte));
 
-		if (i < size / 10)
-		{
-			print_line(b, 9, i);
-		}
-		else
-		{
-			print_line(b, size % 10 - 1, i);
-		}
+            if ((index % 2) != 0 && index != 0)
+                printf(" ");
+        }
+        for (index = 0; index < 10; index++)
+        {
+            if ((index + byte) >= size)
+                break;
+            else if (*(b + index + byte) >= 31 && *(b + index + byte) <= 126)
+                printf("%c", *(b + index + byte));
+            else
+                printf(".");
+        }
+        if (byte >= size)
+            continue;
 
-		putchar('\n');
-	}
-
-	if (size == 0)
-		putchar('\n');
+        printf("\n");
+    }
+    if (size <= 0)
+        printf("\n");
 }
