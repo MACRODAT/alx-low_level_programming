e12f64422f89e143da67367336dbf173a233747c
MACRODAT <macrodatservices@gmail.com>
2023-07-31 17:54:43 +0100

    Changes to the file

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,23 +4,22 @@
  * _strchr - Locates the first occurrence of a character in a string.
  * @s: Pointer to the string to search.
  * @c: The character to be found.
- * 
- * Return: Pointer to the first occurrence of the character in the string, 
- *         or NULL if the character is not found.
+ *
+ * Return: Pointer to the first occurrenc
  */
 char *_strchr(char *s, char c)
 {
-    while (*s != '\0')
-    {
+	while (*s != '\0')
+	{
 		if (*s == c)
 		{
 			return (s);
-        }
-        s++;
-    }
-    if (*s == c)
-    {
-        return (s);
-    }
-    return (NULL);
+		}
+		s++;
+	}
+	if (*s == c)
+	{
+		return (s);
+	}
+	return (NULL);
 }
