f4ac4c4c314ad2acb1d7a90ef072729893814f91
MACRODAT <macrodatservices@gmail.com>
2023-07-31 11:38:27 +0100

    Changes to the file

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * *_memset - fills memory
+ * @s: area
+ * @b: char 
+ * @n: number
+ *
+ * Return: pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	while (n)
+	{
+		*(s + n) = b;
+		n--;
+	}
+	return (s);
+}
