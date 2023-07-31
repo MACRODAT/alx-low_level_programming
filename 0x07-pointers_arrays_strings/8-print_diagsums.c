6826f919df3b121b11f5103d0e89cc0b5b35f5cb
MACRODAT <macrodatservices@gmail.com>
2023-07-31 14:17:55 +0100

    Changes to the file

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums -  prints sums
+ * @a: a pointer
+ * @size: size
+ */
+void print_diagsums(int *a, int size)
+{
+	int i = 0;
+	int sum = 0;
+	int sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum += *(a + (size * i) + i);
+		sum2 += *(a + (size * i) + size - i - 1);
+	}
+	printf("%d,%d\n", sum, sum2);
+}
