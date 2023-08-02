195589c86b6de5e37a4eed6b80f01ac71a6aea1f
MACRODAT <macrodatservices@gmail.com>
2023-08-02 16:43:41 +0100

    Changes to the file

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints
+ * @s: char
+*/
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0' || *s == 0)
+	{
+		return;
+	}
+	_puts_recursion(++s);
+	_putchar(*s);
+}
