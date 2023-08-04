97b090a26424ba9642314bd401404ce1489aa620
MACRODAT <macrodatservices@gmail.com>
2023-08-04 18:38:43 +0100

    Changes to the file

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -10,12 +10,16 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
-
-	for (i = 0; i < argc; i++)
+	if (argc < 2)
 	{
-		printf("%s\n", argv[i]);
+		printf("Error\n");
+		return (1);
 	}
+
+	int i = argv[1];
+	int j = argv[2];
+
+	printf("%d\n", i * j);
 
 	return (0);
 }
