48450090640711c912081413afdf0265d0a2090b
MACRODAT <macrodatservices@gmail.com>
2023-08-04 18:33:06 +0100

    Changes to the file

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prg name
+ * @argc: number
+ * @argv: array
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
