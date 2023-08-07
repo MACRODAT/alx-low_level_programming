30e9f1cd56901ddc4ac09c9d7053bc00c6b0a5f2
MACRODAT <macrodatservices@gmail.com>
2023-08-07 18:33:01 +0100

    Changes to the file

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,7 +12,17 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i = 0;
-	char *arr = malloc(size * sizeof(char));
+	char *arr;
+
+	if (size < 0)
+	{
+		return NULL;
+	}
+	arr = malloc(size * sizeof(char));
+	if (!arr)
+	{
+		return NULL;
+	}
 
 	while (i < size - 1)
 	{
