31213be00d74d6ff77485c479279d1855a413e67
MACRODAT <macrodatservices@gmail.com>
2023-08-08 03:08:11 +0100

    Changes to the file

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -12,7 +12,7 @@ char **strtow(char *str)
 	int i = 0, j = 0, tmp = 0, y = 0;
 	int size = 0, words_l = 0;
 
-	if (str == NULL || str == "")
+	if (str == NULL || *str == "")
 		return (NULL);
 	i = 0;
 	y = 1;
