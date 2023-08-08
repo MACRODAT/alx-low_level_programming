4269f409b94bcb72562c3ccb43ec8e567d049f67
MACRODAT <macrodatservices@gmail.com>
2023-08-08 03:07:27 +0100

    Changes to the file

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,64 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * strtow - frees
+ * @str: grid
+ * Return: n
+ */
+char **strtow(char *str)
+{
+	char **words;
+	int i = 0, j = 0, tmp = 0, y = 0;
+	int size = 0, words_l = 0;
+
+	if (str == NULL || str == "")
+		return (NULL);
+	i = 0;
+	y = 1;
+	while (str[i++])
+	{
+		size++;
+		if (str[i] == ' ')
+		{
+			if (!y)
+				words_l++;
+			y = 1;
+		}
+		else
+		{
+			y = 0;
+		}
+	}
+	i = 0;
+	words = malloc((words_l + 1) * sizeof(char *));
+	if (!words)
+		return (NULL);
+	while (str[j++] == ' ')
+			;
+	j--;
+	tmp = j;
+	while (i < words_l)
+	{
+		while (str[j++] != ' ')
+			;
+		words[i] = malloc((j - tmp + 1) * sizeof(char));
+		if (!words[i])
+			return (NULL);
+		y = 0;
+		while (str[tmp] != ' ')
+		{
+			words[i][y] = str[tmp];
+			tmp++;
+			y++;
+		}
+		words[i][y] = '\0';
+		while (str[j++] == ' ')
+			;
+		j--;
+		tmp = j;
+		i++;
+	}
+	words[i] = '\0';
+	return (words);
+}
