c6fa60ce7fd3f58cb25e7c79a46e32ad0db9d18a
MACRODAT <macrodatservices@gmail.com>
2023-08-11 13:40:10 +0100

    Changes to the file

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,38 +2,43 @@
 #include "main.h"
 
 /**
- * *string_nconcat - str plys str
- * @s1: f
- * @s2: s
- * @n: nbr
+ * *string_nconcat - concatenates n bytes of a string to another string
+ * @s1: string to append to
+ * @s2: string to concatenate from
+ * @n: number of bytes from s2 to concatenate to s1
  *
- * Return: 0
+ * Return: pointer to the resulting string
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s;
-	unsigned int i = 0, j = 0, _l1 = 0, _l2 = 0;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	while (s1 && s1[_l1])
-		_l1++;
-	while (s2 && s2[_l2])
-		_l2++;
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
 
-	if (n >= _l2)
-		s = malloc(sizeof(char) * (_l1 + _l2 + 1));
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
 	else
-		s = malloc(sizeof(char) * (_l1 + n + 1));
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
+
 	if (!s)
 		return (NULL);
-	while (i < _l1)
+
+	while (i < len1)
 	{
-		i++;
 		s[i] = s1[i];
+		i++;
 	}
-	while (i < (_l1 + n) && n < _l2)
+
+	while (n < len2 && i < (len1 + n))
 		s[i++] = s2[j++];
-	while (i < (_l1 + _l2) && n >= _l2)
+
+	while (n >= len2 && i < (len1 + len2))
 		s[i++] = s2[j++];
+
 	s[i] = '\0';
 
 	return (s);
