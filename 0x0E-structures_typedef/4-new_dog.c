56933a165f34e3b621634855b043466cc095b75e
MACRODAT <macrodatservices@gmail.com>
2023-08-15 00:38:45 +0100

    Changes to the file

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,27 @@
 #include "dog.h"
 
 /**
+ * len - pe
+ * @s: str
+ *
+ * Return: lenth
+ */
+int len(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+
+
+/**
  * new_dog - nw
  * @name: name
  * @age: age
@@ -13,12 +34,17 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
 	int i = 0;
+	int l1, l2;
 
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
-	dog->owner = malloc(sizeof(char) * 1000000);
-	dog->name = malloc(sizeof(char) * 1000000);
+	l1 = len(name);
+	l2 = len(owner);
+	dog->name = malloc(sizeof(char) * l1);
+	dog->owner = malloc(sizeof(char) * l2);
+	if (!dog->name || !dog->owner)
+		return (NULL);
 	while (*name)
 	{
 		dog->name[i] = *name;
