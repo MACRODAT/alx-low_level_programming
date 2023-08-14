fd9e27cf557e43023620ba637bf39f26558cef65
MACRODAT <macrodatservices@gmail.com>
2023-08-14 11:52:29 +0100

    Changes to the file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef P1
+#define P1
+
+/**
+ * struct dog - DOG
+ * @name: char
+ * @age: float
+ * @owner: char
+ * 
+ * Description: a dog
+*/
+typedef struct dog {
+	char *name;
+	float age;
+	char *age;
+} dog;
+
+#endif
