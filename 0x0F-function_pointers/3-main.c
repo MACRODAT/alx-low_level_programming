cc4b0db9dba4f72ff39532c45fd714541d0de3f2
MACRODAT <macrodatservices@gmail.com>
2023-08-16 06:09:29 +0100

    Changes to the file

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,4 +1,5 @@
 #include "3-calc.h"
+#include <string.h>
 
 /**
  * main - fct
@@ -9,5 +10,34 @@
 */
 int main(int argc, char *argv[])
 {
-	
+	int a,b,r;
+	int (*f)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	f = get_op_func(argv[2]);
+
+	if (f == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if (b == 0 && (*argv[2] == '/' || *argv[2] == '%'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	r = f(a, b);
+	printf("%d\n", r);
+
+	return (0);
 }
