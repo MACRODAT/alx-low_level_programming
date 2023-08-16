f0bc144be8410f7e2b03db86c9ac41782d93b686
MACRODAT <macrodatservices@gmail.com>
2023-08-16 05:37:38 +0100

    Changes to the file

---
+++ 0x0F-function_pointers/1-array_interator.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - fct
+ * @array: arr
+ * @size: sz
+ * @action: ptr
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i = 0;
+
+	while (i < size)
+	{
+		action(array[i++]);
+	}
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,9 @@
 #ifndef FC
 #define FC
 
+#include <stdlib.h>
+
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
