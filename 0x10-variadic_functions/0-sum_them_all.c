1f54ac2ad9988807d462414958c7328ef142af90
MACRODAT <macrodatservices@gmail.com>
2023-08-17 08:21:55 +0100

    Changes to the file

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,23 +1,25 @@
 #include "variadic_functions.h"
 
 /**
- * sum_them_all - calculates the sum of all its parameters
- * @n: number of arguments passed to the function
+ * sum_them_all - sum
+ * @n: num
  *
- * Return: the resulting sum
+ * Return: sum
  */
 int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i;
 	int sum = 0;
-	va_list list;
+	va_list lst;
 
-	va_start(list, n);
+	va_start(lst, n);
+	while (i < n)
+	{
+		sum += va_arg(lst, int);
+		i++;
+	}
 
-	for (i = 0; i < n; i++)
-		sum += va_arg(list, int);
-
-	va_end(list);
+	va_end(lst);
 
 	return (sum);
 }
