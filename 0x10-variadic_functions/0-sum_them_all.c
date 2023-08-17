749dac4757ff4d5f0f7dd1e10efd7f1c94f33d4d
MACRODAT <macrodatservices@gmail.com>
2023-08-17 08:26:43 +0100

    Changes to the file

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,11 +13,8 @@ int sum_them_all(const unsigned int n, ...)
 	va_list lst;
 
 	va_start(lst, n);
-	while (i < n)
-	{
+	while (i++ < n)
 		sum += va_arg(lst, int);
-		i++;
-	}
 
 	va_end(lst);
 
---
+++ 0x10-variadic_functions/tester.c
@@ -0,0 +1,8 @@
+#include "variadic_functions.h"
+
+int main(void)
+{
+	int sum = sum_them_all(3, 10, 1, 20);
+	printf("sum is %d\n", sum);
+	return 0;	
+}
