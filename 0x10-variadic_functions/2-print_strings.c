34e0486c3b16074757fcf07f6ae74fc3eb80bbac
MACRODAT <macrodatservices@gmail.com>
2023-08-17 08:56:44 +0100

    Changes to the file

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -17,7 +17,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(list, char *);
-		if (!str || !separator)
+		if (!str && !separator)
 			printf("(nil)");
 		else if (!str)
 			printf("(nil)%s", separator);
