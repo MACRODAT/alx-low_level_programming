fdcad28e5c54eeca48b49df7e060317e277c59fb
MACRODAT <macrodatservices@gmail.com>
2023-08-24 22:43:36 +0100

    Changes to the file

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node - Inserts a new node at the start of a linked list
+ * @head: Double pointer to the list_t list
+ * @str: New string to store in the node
+ *
+ * Returns: Address of the new element, or NULL if insertion fails
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newNode;
+	unsigned int strLength = 0;
+
+	while (str[strLength])
+		strLength++;
+
+	newNode = malloc(sizeof(list_t));
+	if (!newNode)
+		return (NULL);
+
+	newNode->str = strdup(str);
+	newNode->len = strLength;
+	newNode->next = (*head);
+	(*head) = newNode;
+
+	return (*head);
+}
