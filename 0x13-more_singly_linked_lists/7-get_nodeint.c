32840182319eee4a251823a6801a81aa09683ae3
MACRODAT <macrodatservices@gmail.com>
2023-08-28 11:58:51 +0100

    Changes to the file

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - ind
+ * @head: first
+ * @index: index
+ *
+ * Return: pointer
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	while (head)
+	{
+		if (i == index)
+			break;
+		i++;
+		head = head->next;
+	}
+	return (head->n);
+}
