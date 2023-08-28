30f726a6e06396208a2a3c2bc0e8eea9f9fbbf2e
MACRODAT <macrodatservices@gmail.com>
2023-08-28 11:52:56 +0100

    Changes to the file

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * pop_listint - pops
+ * @head: ptr
+ *
+ * Return: number
+ */
+int pop_listint(listint_t **head)
+{
+	int number;
+	listint_t *tmp;
+
+	if (!head || !(*head))
+		return (0);
+
+	number = (*head)->n;
+	tmp = *head;
+	*head = (*head)->next;
+	free(tmp);
+
+	return (number);
+}
