2ecfba64d72347bbc8438d90822747c84e5cb9d1
MACRODAT <macrodatservices@gmail.com>
2023-08-28 10:55:57 +0100

    Changes to the file

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_listint - fre
+ * @head: list
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
