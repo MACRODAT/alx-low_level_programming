8ac3a483bdea53df018fc224b24237a09b8edb01
MACRODAT <macrodatservices@gmail.com>
2023-08-30 00:40:56 +0100

    Changes to the file

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,7 +22,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		if (head)
+		{
 			tmp->next = *head;
+			*head = tmp;
+		}
 		return (tmp);
 	}
 	if (!head || !*head)
