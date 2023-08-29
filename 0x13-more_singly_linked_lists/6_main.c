f5b84187ed5b41f8a129229b836dce7dea097d0d
MACRODAT <macrodatservices@gmail.com>
2023-08-30 00:27:25 +0100

    Changes to the file

---
+++ 0x13-more_singly_linked_lists/6_main.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+#include "0-print_listint.c"
+#include "1-listint_len.c"
+#include "10-delete_nodeint.c"
+#include "2-add_nodeint.c"
+#include "3-add_nodeint_end.c"
+#include "4-free_listint.c"
+#include "5-free_listint2.c"
+#include "6-pop_listint.c"
+#include "7-get_nodeint.c"
+#include "8-sum_listint.c"
+#include "9-insert_nodeint.c"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    listint_t *head;
+    int n;
+
+    head = NULL;
+    add_nodeint_end(&head, 0);
+    add_nodeint_end(&head, 1);
+    add_nodeint_end(&head, 2);
+    add_nodeint_end(&head, 3);
+    add_nodeint_end(&head, 4);
+    add_nodeint_end(&head, 98);
+    add_nodeint_end(&head, 402);
+    add_nodeint_end(&head, 1024);
+    print_listint(head);
+    n = pop_listint(&head);
+    printf("- %d\n", n);
+    print_listint(head);
+    n = pop_listint(&head);
+    printf("- %d\n", n);
+    print_listint(head);
+    free_listint2(&head);
+    printf("%p\n", (void *)head);
+    return (0);
+}
