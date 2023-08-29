45e0e7b99c199a5d5cd89934e1d73fae3e4465fc
MACRODAT <macrodatservices@gmail.com>
2023-08-30 00:46:50 +0100

    Changes to the file

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - inserts
+ * @index: node
+ *
+ * Return: node
+*/
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int t = 0;
+	listint_t *next;
+	listint_t *tmp;
+
+	if (index == 0)
+	{
+		if (head)
+		{
+			pop_listint(head);
+			return (1);
+		}
+		return (-1);
+	}
+	if (!head || !*head)
+		return (-1);
+	next = *head;
+	while (next && t < index - 1)
+	{
+		next = next->next;
+		t++;
+	}
+	if (t != index - 1)
+		return (-1);
+	if (next->next)
+	{
+		tmp = next->next;
+		next->next = next-next->next;
+		free(tmp);
+		return (1);
+	}
+	return (-1);
+}
