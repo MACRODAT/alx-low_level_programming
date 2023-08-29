ebdc2aea64cf8b8d83f7d6e5b81c8e17e4ad8ac4
MACRODAT <macrodatservices@gmail.com>
2023-08-30 00:49:06 +0100

    Changes to the file

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -34,7 +34,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (next->next)
 	{
 		tmp = next->next;
-		next->next = next-next->next;
+		next->next = next->next->next;
 		free(tmp);
 		return (1);
 	}
