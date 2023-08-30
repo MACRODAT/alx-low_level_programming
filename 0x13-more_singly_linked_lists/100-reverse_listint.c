7e1346a848c65fcd381d3bdecbe511cd132fd71c
MACRODAT <macrodatservices@gmail.com>
2023-08-30 01:04:41 +0100

    Changes to the file

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - rev
+ * @head: pointer
+ *
+ * Return: pointer
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *next = NULL, *prev = NULL;
+
+	while (*head)
+	{
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = next;
+		*head = next;
+	}
+	return (*head);
+}
