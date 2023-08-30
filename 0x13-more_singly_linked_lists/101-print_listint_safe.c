4180dad4bd623cb6b31580121b529e66201cfd66
MACRODAT <macrodatservices@gmail.com>
2023-08-30 02:05:48 +0100

    Changes to the file

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -2,18 +2,21 @@
 /**
  * print_listint_safe - watches for circular loops
  * @head: pointe
+ *
  * Return: new_nod
  */
 size_t print_listint_safe(const listint_t *head)
 {
 	const listint_t *cur = head;
 	size_t elements = 0;
+	long int diff = 0;
 
 	while (cur)
 	{
 		printf("[%p] %d", (void *)cur, cur->n);
 		elements++;
-		if (cur - cur->next > 0)
+		diff = cur - cur->next;
+		if (diff > 0)
 			cur = cur->next;
 		else
 		{
