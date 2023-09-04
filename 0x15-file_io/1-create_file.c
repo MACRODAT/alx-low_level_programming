c1c4e5f628d7306600d0fd6ea659b321ce660106
MACRODAT <macrodatservices@gmail.com>
2023-09-04 11:33:24 +0100

    Changes to the file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,21 +9,21 @@
 int create_file(const char *filename, char *text_content)
 {
 	int _len = 0, n_write = 0, n_read = 0;
-	char *b;
 
 	if (!filename || !text_content)
 		return (-1);
 	while (text_content[_len])
 		_len++;
 
-	b = malloc(sizeof(char *) * _len);
 	if (!b)
 		return (-1);
 
 	n_read = open(filename, O_CREAT | O_RDWR | O_TRUNC);
-	n_write = write(n_read, b, _len);
+	if (n_read < 0)
+		return (-1);
+	n_write = write(n_read, text_content, _len);
 
-	if (n_read < 0 || n_write < 0 || n_read != n_write)
+	if (n_write < 0 || n_read != n_write)
 		return (-1);
 
 	return (1);
