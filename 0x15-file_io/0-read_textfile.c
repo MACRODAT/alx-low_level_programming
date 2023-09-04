087b0c4b41d0603e51608ab31c19d4b456998477
MACRODAT <macrodatservices@gmail.com>
2023-09-04 10:59:35 +0100

    Changes to the file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file
+ * @filename: file name
+ * @letters: size
+ * Return: nothing
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	int n_read, n_write;
+	char *buffer;
+
+	buffer = malloc(sizeof(char *) * letters);
+	if (!buffer || !filename)
+		return (0);
+
+	fd = open(filename, O_RDWR);
+	n_read = read(fd, buffer, letters);
+	n_write = write(1, buffer, n_read);
+
+	if (fd == -1 || n_read < 0 || n_write < 0 || n_read != n_write)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	close(fd);
+
+	return (n_write);
+}
