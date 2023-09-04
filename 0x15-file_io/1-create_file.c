7c1e5eca916c452cf61bc46cb6095de8f0cf33cd
MACRODAT <macrodatservices@gmail.com>
2023-09-04 11:39:55 +0100

    Changes to the file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,27 +1,31 @@
 #include "main.h"
 
 /**
- * create_file - reads a text file
+ * append_text_to_file - reads a text file
  * @filename: file name
  * @text_content: size
  * Return: nothing
 */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int _len = 0, n_write = 0, n_read = 0;
 
-	if (!filename || !text_content)
+	if (!filename)
 		return (-1);
-	while (text_content[_len])
-		_len++;
 
 	n_read = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (n_read < 0)
 		return (-1);
-	n_write = write(n_read, text_content, _len);
-
-	if (n_write < 0 || n_read != n_write)
-		return (-1);
+	if (!text_content)
+	{
+		while (text_content[_len])
+			_len++;
+		n_write = write(n_read, text_content, _len);
+		if (n_write < 0 || n_read != n_write)
+			return (-1);
+	}
+	
+	close(n_read);
 
 	return (1);
 }
