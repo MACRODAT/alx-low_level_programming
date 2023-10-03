e98d2ef10ca3602f88c1277de0cc60e5af159c3b
MACRODAT <macrodatservices@gmail.com>
2023-10-03 01:22:35 +0100

    Changes to the file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,10 +16,16 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)
+	{
+		free(b);
 		return (0);
+	}
 	read_ = read(fd, b, letters);
 	wrote = write(fd, b, read_);
 	if (read_ < 0 ||  wrote != read_)
+	{
+		free(b);
 		return (0);
+	}
 	return (read_);
 }
