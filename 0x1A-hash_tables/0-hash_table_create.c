73a01a75f0bed53df2d7728dd198d817ae5a444e
MACRODAT <macrodatservices@gmail.com>
2023-11-30 08:39:31 +0100

    Changes to the file

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - new hsh
+ * @size: size
+ * Return: tbl or null
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+	int i = 0;
+
+	if (!size)
+		return (NULL);
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (!table->array)
+		return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		table->array[i] = NULL;
+	}
+	return (table);
+}
