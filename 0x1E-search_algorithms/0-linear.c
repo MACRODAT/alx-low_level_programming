be9c8c3bc4f399ed75ed405f026c9e25ce0d1aee
MACRODAT <macrodatservices@gmail.com>
2024-03-11 13:55:51 +0000

    Changes to the file

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+ * linear_search - looks for number
+ * @array: number
+ * @size: number
+ * @value: value
+ *
+ * Return: int
+*/
+int linear_search(int *array, size_t size, int value)
+{
+	int i = 0;
+
+	if (!array)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
